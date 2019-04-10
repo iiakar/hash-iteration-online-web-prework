378c7c3a6d07608024b399da318df5cda70515ca
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-10 22:50:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,14 +5,7 @@ birthday_kids = {
  }
 
 def happy_birthday(birthday_kids)
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
- 
-winner
+  
 end
 
 
